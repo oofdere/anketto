371fee86e827c7e10f8681d00ec889fca5c73f80
Tibet Tornaci <oofdere@hey.com>
2022-11-19 18:28:23 -0800

    realtime poll updates

--- src/routes/poll/[id]/+page.svelte
+++ src/routes/poll/[id]/+page.svelte
@@ -4,38 +4,46 @@
     export let data: PageData;
     export let form: ActionData;
 
+    let poll: any;
+    const unsubscribe = data.poll.subscribe(value => {
+        poll = value;
+    })
+
     let show_results = false;
 </script>
 
 {#if form?.success || show_results}
 <div class="flex flex-col space-y-1">
-    <h1 class="heading">{data.poll.question}</h1>
+    <h1 class="heading">{poll.question}</h1>
         <ul class="flex flex-col  space-y-1">
-            {#each data.poll.answers as answer, index}
+            {#each poll.answers as answer, index}
                 <li class="relative flex-grow">
                     <div
-                        class="bg-red-500 rounded-md absolute h-full p-2 -z-10"
-                        style="width: {(data.poll.votes[index] / data.poll.total) *
+                        class="transition-all bg-red-500 rounded-md absolute h-full p-2 -z-10"
+                        style="width: {(poll.votes[index] / poll.total) *
                             100}%"
                         aria-hidden="true"
                     />
-                    <div class=" bg-black bg-opacity-10 p-2 rounded-md text-lg">
-                        {answer} / {(
-                            (data.poll.votes[index] / data.poll.total) *
-                            100
-                        ).toFixed(2)}% ({data.poll.votes[index]} votes)
+                    <div class="bg-black bg-opacity-10 p-2 rounded-md text-lg flex">
+                        <span class="flex-grow">{answer}</span>
+                        <span class="place-self-end">
+                            {(
+                                (poll.votes[index] / poll.total) *
+                                100
+                            ).toFixed(2)}%
+                        </span>
                     </div>
                 </li>
             {/each}
+            <p class="supra">{poll.total} VOTES</p>
         </ul>
-    </div>    
-
+    </div>
 {:else}
     <div class="flex flex-col space-y-1">
-        <h1 class="heading">{data.poll.question}</h1>
+        <h1 class="heading">{poll.question}</h1>
         <form class="flex flex-col space-y-1" action="?/vote" method="POST">
             <ul class="flex flex-col  space-y-1">
-                {#each data.poll.answers as answer, index}
+                {#each poll.answers as answer, index}
                     <li class="bg-gray-100 p-2 rounded-md text-lg flex-grow">
                         <input type="radio" name="vote" value={index} />
                         <label for={index.toString()}>{answer}</label>
--- src/routes/poll/[id]/+page.ts
+++ src/routes/poll/[id]/+page.ts
@@ -1,13 +1,14 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import { browser } from "$app/environment";
+import { writable } from "svelte/store";
 
 import PocketBase from "pocketbase";
 const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 export const load: PageLoad = async ({params}) => {
     // get poll from PocketBase, SSR and client-side render supported
-    
     let record;
     try {
         record = await pb.collection('polls').getOne(params.id);
@@ -15,13 +16,29 @@ export const load: PageLoad = async ({params}) => {
         throw error(404);
     };
 
+    let poll = {
+        question: record.question,
+        answers: record.answers,
+        votes: record.votes,
+        total: record.total_votes
+    }
+
+    const realtime = writable(poll);
+
+    if (browser) {
+        pb.collection('polls').subscribe(params.id, function (e) {
+            let poll = {
+                question: e.record.question,
+                answers: e.record.answers,
+                votes: e.record.votes,
+                total: e.record.total_votes
+            };
+            realtime.set(poll);
+        });
+    }
+
     return {
-        poll: {
-            question: record.question,
-            answers: record.answers,
-            votes: record.votes,
-            total: record.total_votes
-        }
+        poll: realtime
     };
 }
 
@@ -32,12 +49,4 @@ export const load: PageLoad = async ({params}) => {
 
 
 
-
-import { redirect } from "@sveltejs/kit";
-
-
-import type { PageData } from "./$types";
-export let data: PageData;
-
-let record;
 
